0bbc321ade1cced18546422870c2a1057b531b95
rasvv <rasvv@mail.ru>
2021-02-26 16:47:21 +0300

    Сделал перевод на промис

--- js/main.js
+++ js/main.js
@@ -1,26 +1,39 @@
 'use strict'
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
+////////////////////////////////////////////////////////////////////////////////
+// let getRequest = (url, callBack) => {
+// 	let xhr = new XMLHttpRequest();
+// 	xhr.open('GET', url, true);
+// 	xhr.onreadystatechange = () => {
+// 		if (xhr.readyState === 4) {
+// 			if (xhr.status !== 200) {
+// 				console.log('Error');
+// 			} else {
+// 				callBack(xhr.responseText);
+// 			}
+// 		}
+// 	}
+// 	xhr.send();
+// };
 
-let getRequest = (url, callBack) => {
-	let xhr = new XMLHttpRequest();
-	xhr.open('GET', url, true);
-	xhr.onreadystatechange = () => {
-		if (xhr.readyState === 4) {
-			if (xhr.status !== 200) {
-				console.log('Error');
-			} else {
-				callBack(xhr.responseText);
-			}
-		}
-	}
-	xhr.send();
-};
 
 
+/////////////////////////////////////////////////////////////////////////////////
+//  Завернул предыдущую функцию в промис
+let getRequest = (url) => {
+	return new Promise((resolve, reject) => {
+	  const xhr = new XMLHttpRequest();
+	  xhr.open("GET", url, true);
+	  xhr.onload = () => resolve(xhr.responseText);
+	  xhr.onerror = () => reject(xhr.statusText);
+	  xhr.send();
+	});
+ }
+//////////////////////////////////////////////////////////////////////////////////
 
 class ProductsItem{
-	constructor(product, img = '<img src="https://picsum.photos/200/200?random=${Math.random()}" alt="img"></img>'){
+	constructor(product, img = `<img src="https://picsum.photos/200/200?random=${Math.random()}" alt="img"></img>`){
 		this.title = product.product_name;
 		this.price = product.price;
 		this.id = product.id;
@@ -50,12 +63,12 @@ class ProductsList {
 		this.#products = [];
 		this.#allProducts = [];
 
-		// this.#fetchProducts();
-		this.#getProducts()
-		.then((data) => {
-			this.#products = [...data];
-			this.#render();
-		});
+		this.#fetchProducts();
+		// this.#getProducts()
+		// .then((data) => {
+		// 	this.#products = [...data];
+		// 	this.#render();
+		// });
 
 	}
 
@@ -68,14 +81,68 @@ class ProductsList {
      return priceWithDiscpunt;
   	}
 
-	// #fetchProducts() {
-	// 	getRequest(`${API}/catalogData.json`, (data) => {
-	// 		console.log(data);
-	// 		this.#products = JSON.parse(data);
-	// 		this.#render();
-	// 		this.showSum();
+///////////////////////////////////////////////////////////////////////////////
+	//Вызов промиса
+	#fetchProducts() {
+		getRequest(`${API}/catalogData.json`).then((data) => {
+			console.log(data);
+			this.#products = JSON.parse(data);
+			this.#render();
+			this.showSum();
+		});
+	}
+////////////////////////////////////////////////////////////////////////////////
+
+	// #getProducts () {
+	// 	return fetch(`${API}/catalogData.json`)
+	// 	.then((response) => response.json())
+	// 	.catch((err) => {
+	// 		console.log(err);
 	// 	});
 	// }
+
+
+	#render() {
+		const listHTML = document.querySelector(this.container);
+		this.#products.forEach((product) => {
+			const productsItem = new ProductsItem(product);
+			console.log(productsItem);
+			this.#allProducts.push(productsItem);
+			listHTML.insertAdjacentHTML('beforeend', productsItem.render());
+		});
+	}
+
+	showSum() {
+		this.totalPrice.innerHTML = `Суммарная стоимость всех товаров = ${this.sum()} рублей`;
+		let discount = 10;
+		this.discountPrice.innerHTML = `Стоимость со скидкой ${discount}% = ${this.getTotalWithDiscount(discount)} рублей`;		
+	}
+
+}
+
+const productsList = new ProductsList();
+
+
+
+class CartList extends ProductsList {
+	#products;
+	#allProducts;
+	totalPrice = document.querySelector('.totalprice');
+	discountPrice = document.querySelector('.discountprice');
+
+	constructor(container = '.cart__list') {
+		this.container = container;
+		this.#products = [];
+		this.#allProducts = [];
+
+		// this.#fetchProducts();
+		this.#getProducts()
+		.then((data) => {
+			this.#products = [...data];
+			this.#render();
+		});
+
+	}
 
 	#getProducts () {
 		return fetch(`${API}/catalogData.json`)
@@ -104,4 +171,4 @@ class ProductsList {
 
 }
 
-const productsList = new ProductsList();
+const cartList = new CartList();
